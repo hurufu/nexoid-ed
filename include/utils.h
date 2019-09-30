50428cb246606b82e98034e29f7c42f2f2c8dd23
Aleksy Grabowski <hurufu@gmail.com>
2019-09-30 12:45:02 +0300

    Use ONEOFx macros

--- include/utils.h
+++ include/utils.h
@@ -10,6 +10,12 @@
 
 #define STRING_SENTINEL struct { char : 1; }
 
+// TODO: Replace ONEOF macro with proper map/reduce equivalent
+#define ONEOF1(X, A1)             (((X) == (A1)))
+#define ONEOF2(X, A1, A2)         (((X) == (A1)) || ((X) == (A2)))
+#define ONEOF3(X, A1, A2, A3)     (((X) == (A1)) || ((X) == (A2)) || ((X) == (A3)))
+#define ONEOF4(X, A1, A2, A3, A4) (((X) == (A1)) || ((X) == (A2)) || ((X) == (A3)) || ((X) == (A4)))
+
 struct string16 {
     char s[16];
     STRING_SENTINEL;

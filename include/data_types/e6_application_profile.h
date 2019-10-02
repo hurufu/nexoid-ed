7a15b66029e2ce0b0e8943da040c76d03c789839
Aleksy Grabowski <hurufu@gmail.com>
2019-10-02 18:22:57 +0300

    Add Effective AP

--- include/data_types/e6_application_profile.h
+++ include/data_types/e6_application_profile.h
@@ -7,7 +7,7 @@
 // TODO: Add tlv retrieval by tag
 // FIXME: Consider header generation from ASN.1 module
 
-struct ApplicationProfile {
+struct ApplicationProfileBase {
     // Unique Application Profile ID:
     bcd_t profileNumber;
     bcd_t acquirerNumber;
@@ -28,7 +28,20 @@ struct ApplicationProfile {
     union MagStripeCvmCapability* magStripeCvmCapabilityCvmRequired;
     union MagStripeCvmCapability* magStripeCvmCapabilityNoCvmRequired;
 
+    // missing Default DDA DOL (DF1A)
     bcd_t holdTimeValue;
     union Kernel2Configuration* kernel2Configuration;
     union Kernel4ReaderCapabilities* kernel4ReaderCapabilities;
+    union Kernel4Settings* kernel4Settings;
+    union MsrCvmCapability* msrCvmCapabilityCvmRequired;
+    union MsrCvmCapability* msrCvmCapabilityNoCvmRequired;
+    struct bcd2* maxTargetPercentageForBiasedRandomSelection;
+    struct bcd4 merchantCategoryCode;
+    uint8_t (* merchantCustomData)[20];
+    char merchantIdentifier[15];
+    char* merchantNameAndLocation;
+};
+
+struct ApplicationProfile {
+    struct ApplicationProfileBase;
 };
---
+++ include/data_types/effective_application_profile.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "e6_application_profile.h"
+
+struct EffectiveApplicationProfile {
+    struct ApplicationProfileBase;
+};

8d150c1e98cf26d8a0fb5d70bf15696296b8b237
Aleksy Grabowski <hurufu@gmail.com>
2019-11-29 19:14:45 +0100

    Add function for getting tag value for AP

--- application_profile.c
+++ application_profile.c
@@ -1,22 +1,61 @@
-#include "include/tag_retrival.h"
-#include "include/application_profile.h"
+#include "tag_retrival.h"
+#include "e6_application_profile.h"
+#include "global_data_elements.h"
 
-struct tlv*
-ApplicationProfile_gettag(
-        const struct ApplicationProfile* const p
-      , const enum ApplicationProfileTag t
-) {
-    switch (p) {
-        case TAG_EMV_ACQUIRER_IDENTIFIER:
-            return MAKE_TAG(p, &p->acquirerIdentifier);
-        case TAG_EMV_CVM_CAPABILITY_CVM_REQUIRED:
-            return MAKE_TAG(p, &p->cvmCapabilityCvmRequired);
-        case TAG_EMV_CVM_CAPABILITY_NO_CVM_REQUIRED:
-            return MAKE_TAG(p, &p->cvmCapabilityNoCvmRequired);
-        case TAG_EMV_DDOL:
-            return MAKE_TAG(p, &p->ddol);
-        case TAG_EMV_HOLD_TIME_VALUE:
-            return MAKE_TAG(p, &p->holdTimeValue);
+#define T(T) T
+
+static struct TagTypePointer ApplicationProfile_get_ttv(const struct ApplicationProfile* const p, const union TagExpanded t) {
+    struct TagTypePointer ret;
+    switch (t.i) {
+        case T(0xDF1B): ret = (struct TagTypePointer){ .type = TAG_BCD1, .value.bcd = &p->acquirerNumber }; break;
+        case T(0xDF19): ret = (struct TagTypePointer){ .type = TAG_BCD1, .value.bcd = &p->profileNumber }; break;
+        case T(0xDF26): ret = (struct TagTypePointer){ .type = TAG_TVR_PTR, .value.tvr = p->additionalRestrictionsForForcedAcceptance }; break;
+        case T(0x9F40): ret = (struct TagTypePointer){ .type = TAG_ATC_PTR, .value.atc = p->additionalTerminalCapabilities }; break;
+        case T(0x9F33): ret = (struct TagTypePointer){ .type = TAG_TC_PTR, .value.tc = p->terminalCapabilities }; break;
+        case T(0x9F1A): ret = (struct TagTypePointer){ .type = TAG_BCD2, .value.bcd2 = &p->terminalCountryCode }; break;
+        case T(0xDF41): ret = (struct TagTypePointer){ .type = TAG_ANS_16, .value.ans_16 = &p->applicationLabelDefault }; break;
+        case T(0xDF27): ret = (struct TagTypePointer){ .type = TAG_APS, .value.aps = &p->applicationProfileSettings }; break;
+        case T(0xDF28): ret = (struct TagTypePointer){ .type = TAG_APSC_PTR, .value.apsc = p->applicationProfileSettingsForCancellation }; break;
+        case T(0xDF29): ret = (struct TagTypePointer){ .type = TAG_AMOUNT_PTR, .value.amount = p->cashAdvanceMaximumAmount }; break;
+        case T(0xDF2A): ret = (struct TagTypePointer){ .type = TAG_AMOUNT_PTR, .value.amount = p->cashbackMaximumAmount }; break;
+        case T(0xDF8118): ret = (struct TagTypePointer){ .type = TAG_CVM_CAPABILITY_PTR, .value.cvmCapability = p->cvmCapabilityCvmRequired }; break;
+        case T(0xDF8119): ret = (struct TagTypePointer){ .type = TAG_CVM_CAPABILITY_PTR, .value.cvmCapability = p->cvmCapabilityNoCvmRequired }; break;
+        case T(0xDF42): ret = (struct TagTypePointer){ .type = TAG_MSR_CVM_CAPABILITY_PTR, .value.magstripeCvmCapability = p->magStripeCvmCapabilityCvmRequired }; break;
+        //case T(): ret = (struct TagTypePointer){ .type = TAG_MSR_CVM_CAPABILITY_PTR, .value.magstripeCvmCapability = p->magStripeCvmCapabilityNoCvmRequired }; break;
+        case T(0xDF58): ret = (struct TagTypePointer){ .type = TAG_AMOUNT_PTR, .value.amount = p->cvcDefaultAmount }; break;
+        case T(0xDF3C): ret = (struct TagTypePointer){ .type = TAG_AMOUNT, .value.amount = &p->refundProtectionAmount }; break;
+        case T(0xDF811B): ret = (struct TagTypePointer){ .type = TAG_KERNEL2_CONFIGURATION_PTR, .value.kernel2Configuration = p->kernel2Configuration}; break;
+        default: ret = (struct TagTypePointer){ .type = TAG_UNKNOWN }; break;
     }
-    return NULL;
+    return ret;
+}
+
+uint8_t* ApplicationProfile_getv(const struct ApplicationProfile* const ap, const union TagExpanded tag) {
+    const struct TagTypePointer t = ApplicationProfile_get_ttv(ap, tag);
+    switch (t.type) {
+        case TAG_BOOLEAN: return getv_bool(t.value.b);
+        case TAG_BOOLEAN_PTR: return t.value.b ? getv_bool(t.value.b) : NULL;
+        case TAG_C19: return getv_c19(t.value.c19);
+        case TAG_BID: return getv_bid(t.value.bid);
+        case TAG_BCD1: return getv_bcd(t.value.bcd);
+        case TAG_BCD2: return getv_bcd2(t.value.bcd2);
+        case TAG_ANS_16: return getv_ans_16(t.value.ans_16);
+        case TAG_TVR: return getv_tvr(t.value.tvr);
+        case TAG_TVR_PTR: return t.value.tvr ? getv_tvr(t.value.tvr) : NULL;
+        case TAG_ATC: return getv_atc(t.value.atc);
+        case TAG_ATC_PTR: return t.value.atc ? getv_atc(t.value.atc) : NULL;
+        case TAG_TC: return getv_tc(t.value.tc);
+        case TAG_TC_PTR: return t.value.tc ? getv_tc(t.value.tc) : NULL;
+        case TAG_APS: return getv_aps(t.value.aps);
+        case TAG_APSC_PTR: return t.value.apsc ? getv_apsc(t.value.apsc) : NULL;
+        case TAG_AMOUNT: return getv_amount(t.value.amount);
+        case TAG_AMOUNT_PTR: return t.value.amount ? getv_amount(t.value.amount) : NULL;
+        case TAG_CVM_CAPABILITY_PTR: return t.value.cvmCapability ? getv_cvmCapability(t.value.cvmCapability) : NULL;
+        case TAG_MSR_CVM_CAPABILITY_PTR: return t.value.magstripeCvmCapability ? getv_msrCvmCapability(t.value.magstripeCvmCapability) : NULL;
+        case TAG_KERNEL2_CONFIGURATION_PTR: return t.value.kernel2Configuration ? getv_kernel2Configuration(t.value.kernel2Configuration) : NULL;
+        case TAG_UNKNOWN:
+        case TAG_FORBIDDEN:
+        default:
+            return NULL;
+    }
 }
--- include/tag_retrival.h
+++ include/tag_retrival.h
@@ -1,13 +1,179 @@
 #pragma once
 
-struct tlv {
-    union {
-        uint8_t head;
-        struct {
-            uint8_t nmbr : 5;
-            uint8_t tmpl : 1;
-            uint8_t type : 2;
-        };
-    };
-    uint8_t tail[];
+#include "common.h"
+#include "c2.h"
+
+typedef uintmax_t tlv_tag_t;
+typedef size_t tlv_size_t;
+
+enum TagType {
+    TAG_UNKNOWN
+  , TAG_BOOLEAN
+  , TAG_BOOLEAN_PTR
+  , TAG_C19
+  , TAG_BID
+  , TAG_BCD1
+  , TAG_BCD2
+  , TAG_ANS_16
+  , TAG_TVR
+  , TAG_TVR_PTR
+  , TAG_ATC
+  , TAG_ATC_PTR
+  , TAG_TC
+  , TAG_TC_PTR
+  , TAG_APS
+  , TAG_APSC_PTR
+  , TAG_AMOUNT
+  , TAG_AMOUNT_PTR
+  , TAG_CVM_CAPABILITY_PTR
+  , TAG_MSR_CVM_CAPABILITY_PTR
+  , TAG_KERNEL2_CONFIGURATION_PTR
+  , TAG_FORBIDDEN
 };
+
+union TagExpanded {
+    uint8_t raw[sizeof(tlv_tag_t)];
+    tlv_tag_t i;
+    struct {
+        union {
+            uint8_t head;
+            struct {
+                uint8_t nmbr : 5;
+                uint8_t constructed : 1;
+                enum {
+                    TLV_CLASS_UNIVERSAL = 0b00,
+                    TLV_CLASS_APPLICATION = 0b01,
+                    TLV_CLASS_CONTEXT_SPECIFIC = 0b10,
+                    TLV_CLASS_PRIVATE = 0b11
+                } type : 2;
+            };
+        };
+        uint8_t tail[sizeof(tlv_tag_t) - sizeof(uint8_t)];
+    };
+};
+
+struct TagTypePointer {
+    enum TagType type;
+    union {
+        const bool* b;
+        const char (*c19)[19];
+        const struct Bid* bid;
+        const bcd_t* bcd;
+        const struct bcd2* bcd2;
+        const struct ans_16* ans_16;
+        const union TerminalVerificationResults* tvr;
+        const union AdditionalTerminalCapabilities* atc;
+        const union TerminalCapabilities* tc;
+        const union ApplicationProfileSettings* aps;
+        const union ApplicationProfileSettingsForCancellation* apsc;
+        const union Amount* amount;
+        const union CvmCapability* cvmCapability;
+        const union MagStripeCvmCapability* magstripeCvmCapability;
+        const union Kernel2Configuration* kernel2Configuration;
+    } value;
+};
+
+static inline
+uint8_t* getv_bool(const bool* v) {
+    uint8_t* const ret = mempool_malloc(sizeof(*v));
+    *ret = *v;
+    return ret;
+}
+
+static inline
+uint8_t* getv_c19(const char (*const v)[19]) {
+    uint8_t* const ret = mempool_malloc(sizeof(*v));
+    memcpy(ret, *v, sizeof(*v));
+    return ret;
+}
+
+static inline
+uint8_t* getv_bid(const struct Bid* const v) {
+    uint8_t* const ret = mempool_malloc(v->size);
+    memcpy(ret, v->value, v->size);
+    return ret;
+}
+
+static inline
+uint8_t* getv_bcd(const bcd_t* const v) {
+    uint8_t* const ret = mempool_malloc(sizeof(*v));
+    *ret = *v;
+    return ret;
+}
+
+static inline
+uint8_t* getv_bcd2(const struct bcd2* const v) {
+    uint8_t* const ret = mempool_malloc(sizeof(*v));
+    memcpy(ret, v->v, sizeof(v->v));
+    return ret;
+}
+
+static inline
+uint8_t* getv_ans_16(const struct ans_16* const v) {
+    uint8_t* const ret = mempool_malloc(v->l);
+    memcpy(ret, v->v, v->l);
+    return ret;
+}
+
+static inline
+uint8_t* getv_tvr(const union TerminalVerificationResults* const v) {
+    uint8_t* const ret = mempool_malloc(sizeof(*v));
+    memcpy(ret, v->raw, sizeof(v->raw));
+    return ret;
+}
+
+static inline
+uint8_t* getv_atc(const union AdditionalTerminalCapabilities* const v) {
+    uint8_t* const ret = mempool_malloc(sizeof(*v));
+    memcpy(ret, v->raw, sizeof(v->raw));
+    return ret;
+}
+
+static inline
+uint8_t* getv_tc(const union TerminalCapabilities* const v) {
+    uint8_t* const ret = mempool_malloc(sizeof(*v));
+    memcpy(ret, v->raw, sizeof(v->raw));
+    return ret;
+}
+
+static inline
+uint8_t* getv_aps(const union ApplicationProfileSettings* const v) {
+    uint8_t* const ret = mempool_malloc(sizeof(*v));
+    memcpy(ret, v->raw, sizeof(v->raw));
+    return ret;
+}
+
+static inline
+uint8_t* getv_apsc(const union ApplicationProfileSettingsForCancellation* const v) {
+    uint8_t* const ret = mempool_malloc(sizeof(*v));
+    memcpy(ret, v->raw, sizeof(v->raw));
+    return ret;
+}
+
+static inline
+uint8_t* getv_amount(const union Amount* const v) {
+    uint8_t* const ret = mempool_malloc(sizeof(*v));
+    memcpy(ret, v->bcd, sizeof(v->bcd));
+    return ret;
+}
+
+static inline
+uint8_t* getv_cvmCapability(const union CvmCapability* const v) {
+    uint8_t* const ret = mempool_malloc(sizeof(*v));
+    memcpy(ret, v->raw, sizeof(v->raw));
+    return ret;
+}
+
+static inline
+uint8_t* getv_msrCvmCapability(const union MagStripeCvmCapability* const v) {
+    uint8_t* const ret = mempool_malloc(sizeof(*v));
+    memcpy(ret, v->raw, sizeof(v->raw));
+    return ret;
+}
+
+static inline
+uint8_t* getv_kernel2Configuration(const union Kernel2Configuration* const v) {
+    uint8_t* const ret = mempool_malloc(sizeof(*v));
+    memcpy(ret, v->raw, sizeof(v->raw));
+    return ret;
+}

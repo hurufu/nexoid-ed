a22b630ca1020e91674c083f686d0119a1332ad4
Aleksy Grabowski <hurufu@gmail.com>
2019-12-28 12:44:56 +0100

    Small cosmetical change in common header

--- common.h
+++ common.h
@@ -5,9 +5,6 @@
 
 #define lastof(Array) ((Array) + sizeof(Array) - 1)
 
-#define ck_assert_tl(Tl, Result, Length, BytesLeft, NextCursor, ...)\
-    ck_assert_tl_impl((Tl), (Result), (&(uint8_t[sizeof(tlv_tag_t)]){ __VA_ARGS__}), (Length), (BytesLeft), (NextCursor))
-
 #define ck_assert_op(Op, A, B, AString, BString)\
     ck_assert_msg((A) Op (B), "Assertion '"#A" "#Op" "#B"' failed. "#A" == %s and "#B" == %s", AString, (BString))
 
@@ -20,11 +17,15 @@
 #define ck_assert_dol_result_neq(A, B)\
     ck_assert_dol_result(!=, A, B)
 
+#define ck_assert_tl(Tl, Result, Length, BytesLeft, NextCursor, ...)\
+    ck_assert_tl_impl((Tl), (Result), (&(uint8_t[sizeof(tlv_tag_t)]){ __VA_ARGS__ }), (Length), (BytesLeft), (NextCursor))
 void ck_assert_tl_impl(
     struct Extracted_Tl tl,
     enum ProcedureResult expected_result,
-    const uint8_t (*const tag)[sizeof(tlv_tag_t)],
+    const uint8_t (*tag)[sizeof(tlv_tag_t)],
     size_t length,
     size_t bytes_left,
     const uint8_t* next_cursor
 );
+
+static const char* dol_result_tostring(enum ProcedureResult r);

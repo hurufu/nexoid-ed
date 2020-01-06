eb1fc645345aaa295d249549ff5332eb148387a4
Aleksy Grabowski <hurufu@gmail.com>
2020-01-06 12:58:20 +0100

    Add declineDisplayMessageId to the ord

--- include/global_data_elements/online_response_data.h
+++ include/global_data_elements/online_response_data.h
@@ -13,4 +13,21 @@ struct OnlineResponseData {
     enum AuthorisationResponseCode authorisationResponseCode;
     char declineDisplayMessage[40 + 1];
     struct IssuerAuthenticationData* iatd;
+
+    // WARNING: declineDisplayMessageId isn't defined by nexo.
+    //
+    // According to nexo-FAST v.3.2, section 13.3.50 type of Decline Display
+    // Message should be an...40, but in section 12.1.3.2 it's mentioned that
+    // HAP in some circumstances should set it to Cardholder Message 0x1D,
+    // which is inconsistent with it's definition or common software
+    // engeneering practice under any interpetation of the passage. Namely if
+    // we interpret it as if HAP should populate this data element with proper
+    // translated text based on message id 0x1D, then suddenly HAP needs access
+    // to all (or partial) cardholder or attendant translations, which is in
+    // scope of SCAP only (no citation), but if we interpret it as if HAP
+    // should copy byte 0x1D verbatim into this data element then it's
+    // inconsitent with it's definition, because 0x1D isn't an alphanumeric
+    // character. As a temporary workaround new optional data element is added:
+    // Decline Display Message Id that can hold Cardholder Message Id
+    enum CardholderMessage* declineDisplayMessageId;
 };
